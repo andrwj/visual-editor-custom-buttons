3eff9c5b0c5b67e7bce281ba23f945b27eb7cada
A.J <andrwj@gmail.com>
2019-08-10 01:06:42 +0900

    Editor 안에서 PrismJS 적용시 배경색이 제대로 처리되게 함

--- js/button-1-9.js
+++ js/button-1-9.js
@@ -17,12 +17,15 @@ let is_prismjs_loaded = false;
           const doc = ed.dom.doc;
           const head = doc.getElementsByTagName('head')[0];
           const base ='/wp-content/plugins/prismatic/lib/prism/';
-
+					const lastWPstyleNode = [...head.childNodes]
+					                          .filter(node => node.rel === 'stylesheet' && /wp\-custom\-style\.css/.test(node.href) )
+																		.pop();
           const link = document.createElement("link");
           link.setAttribute("rel", "stylesheet");
           link.setAttribute("type", "text/css");
           link.setAttribute("href", `${base}/css/theme-funky.css`);
-          head.appendChild(link);
+					// 이 위치에 넣지 않으면 Editor 안에서 <pre><code> 블록의 바탕색이 흰색으로 override 된다.
+          head.insertBefore(link, lastWPstyleNode);
 
           ['prism-core.js', 'plugin-autoloader.js', 'plugin-toolbar.min.js',
            'plugin-normalize-whitespace.min.js', 'plugin-line-numbers.min.js', 'plugin-copy-to-clipboard.min.js'].forEach((lib) => {
