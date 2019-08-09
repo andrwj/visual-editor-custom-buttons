25ba1b3012fa5616eaedaac1f5c29d6677b731fc
A.J <andrwj@gmail.com>
2019-08-10 01:30:40 +0900

    PrismJS 자동로딩하도록 수정

--- js/button-1-9.js
+++ js/button-1-9.js
@@ -6,37 +6,41 @@ function getBaseURL () {
 }
 
 let is_prismjs_loaded = false;
+function load_prismjs_stuffs(ed) {
+  if(is_prismjs_loaded) return;
+
+  const doc = ed.dom.doc;
+  const head = doc.getElementsByTagName('head')[0];
+  const base ='/wp-content/plugins/prismatic/lib/prism/';
+	const lastWPstyleNode = [...head.childNodes]
+        .filter(node => node.rel === 'stylesheet' && /wp\-custom\-style\.css/.test(node.href) )
+        .pop();
+  const link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("href", `${base}/css/theme-funky.css`);
+	// 이 위치에 넣지 않으면 Editor 안에서 <pre><code> 블록의 바탕색이 흰색으로 override 된다.
+  head.insertBefore(link, lastWPstyleNode);
+
+  ['prism-core.js', 'plugin-autoloader.js', 'plugin-toolbar.min.js',
+   'plugin-normalize-whitespace.min.js', 'plugin-line-numbers.min.js', 'plugin-copy-to-clipboard.min.js'].forEach((lib) => {
+     const script = doc.createElement('script');
+     script.type = 'text/javascript';
+     script.async = false;
+     script.src = `${base}/js/${lib}`;
+     head.appendChild(script);
+   });
+  is_prismjs_loaded = true;
+}
+
 
 (function() {
   tinymce.create('tinymce.plugins.vecb_button9', {
     init : function(ed, url) {
       ed.addButton('vecb_button9', {
-        title : 'Apply PrismJS',image : url+'/icons/magic.png',onclick : function() {
-          if(is_prismjs_loaded) return;
-
-          const doc = ed.dom.doc;
-          const head = doc.getElementsByTagName('head')[0];
-          const base ='/wp-content/plugins/prismatic/lib/prism/';
-					const lastWPstyleNode = [...head.childNodes]
-					                          .filter(node => node.rel === 'stylesheet' && /wp\-custom\-style\.css/.test(node.href) )
-																		.pop();
-          const link = document.createElement("link");
-          link.setAttribute("rel", "stylesheet");
-          link.setAttribute("type", "text/css");
-          link.setAttribute("href", `${base}/css/theme-funky.css`);
-					// 이 위치에 넣지 않으면 Editor 안에서 <pre><code> 블록의 바탕색이 흰색으로 override 된다.
-          head.insertBefore(link, lastWPstyleNode);
-
-          ['prism-core.js', 'plugin-autoloader.js', 'plugin-toolbar.min.js',
-           'plugin-normalize-whitespace.min.js', 'plugin-line-numbers.min.js', 'plugin-copy-to-clipboard.min.js'].forEach((lib) => {
-             const script = doc.createElement('script');
-             script.type = 'text/javascript';
-             script.async = false;
-             script.src = `${base}/js/${lib}`;
-             head.appendChild(script);
-           });
-        }
+        title : 'Apply PrismJS',image : url+'/icons/magic.png', onclick : () => load_prismjs_stuffs(ed)
       });
+      setTimeout(() => load_prismjs_stuffs(ed), 1000)
     },
     createControl : function(n, cm) {
       return null;
