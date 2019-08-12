d1109a1eef008e3b66ca162cc83922530dc045ca
A.J <andrwj@gmail.com>
2019-08-12 09:26:32 +0900

    tracking-mark 버튼 추가

---
+++ js/button-1-18.js
@@ -0,0 +1,6 @@
+(function() {
+                tinymce.create('tinymce.plugins.vecb_button18', {init : function(ed, url) {ed.addButton('vecb_button18', {title : 'Tracking Mark',image : 'http://andrwj.net/wp-content/uploads/vecb/tracking-40x40.png',onclick : function() {
+                ed.selection.setContent('<span class="tracking-mark">↳</span>');}});
+            }, createControl : function(n, cm) {return null;},});
+                tinymce.PluginManager.add('vecb_button18', tinymce.plugins.vecb_button18);
+	    })();
