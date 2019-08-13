4cc8ed87e6b00f7af111c27f5aff0a77ef7c7d71
A.J <andrwj@gmail.com>
2019-08-13 21:44:02 +0900

    모든 버튼 뒤에 공백을 삽입할 수 있게 표식을 추가

--- js/button-1-17.js
+++ js/button-1-17.js
@@ -3,7 +3,7 @@
     init : function(ed, url) {
       ed.addButton('vecb_button17', {
         title : 'SquareBox',image : url+'/icons/box.png', onclick : function() {
-              ed.selection.setContent('<span class="square-box">' + ed.selection.getContent() + '</span>');
+              ed.selection.setContent('<span class="square-box">' + ed.selection.getContent() + '</span> |');
          }
       });
     },
--- js/button-1-18.js
+++ js/button-1-18.js
@@ -1,6 +1,6 @@
 (function() {
                 tinymce.create('tinymce.plugins.vecb_button18', {init : function(ed, url) {ed.addButton('vecb_button18', {title : 'Tracking Mark',image : 'http://andrwj.net/wp-content/uploads/vecb/tracking-40x40.png',onclick : function() {
-                ed.selection.setContent('<span class="tracking-mark">↳</span>');}});
+                ed.selection.setContent('<span class="tracking-mark">↳</span> |');}});
             }, createControl : function(n, cm) {return null;},});
                 tinymce.PluginManager.add('vecb_button18', tinymce.plugins.vecb_button18);
 	    })();
